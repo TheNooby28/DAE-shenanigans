895b9fd8cd55697d14a27aff075cb4d99e05c1d0
TheNooby28 <jayjadamoski@gmail.com>
2025-07-23 13:27:49 -0400

    deleted testApi script

--- python_1/quoteApiTest.py
+++
@@ -1,7 +0,0 @@
-import requests
-api_url = 'https://api.api-ninjas.com/v1/quotes'
-response = requests.get(api_url, headers={'X-Api-Key': 'YOUR_API_KEY'})
-if response.status_code == requests.codes.ok:
-    print(response.text)
-else:
-    print("Error:", response.status_code, response.text)
