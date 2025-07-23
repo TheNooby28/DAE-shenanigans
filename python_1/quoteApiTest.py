1e3e2ef4ec35aac73d668f781975a7793c992b88
TheNooby28 <jayjadamoski@gmail.com>
2025-07-23 13:24:07 -0400

    added research document to design folder

---
+++ python_1/quoteApiTest.py
@@ -0,0 +1,7 @@
+import requests
+api_url = 'https://api.api-ninjas.com/v1/quotes'
+response = requests.get(api_url, headers={'X-Api-Key': 'YOUR_API_KEY'})
+if response.status_code == requests.codes.ok:
+    print(response.text)
+else:
+    print("Error:", response.status_code, response.text)
